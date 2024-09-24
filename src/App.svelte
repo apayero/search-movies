5029502d40c34ff2507ae412d10e12d63dbc5b71
apayero <atpayero@gmail.com>
2024-09-24 01:51:30 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Counter from "./Counter.svelte";
+</script>
+
+<Counter />
+<Counter />
+<Counter />
+<span>Tenemos counters aquí</span>
+
+<style>
+  span {
+    color: #09f;
+  }
+</style>
---
+++ src/Counter.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let initialCounter = 0;
+  let contador = initialCounter;
+
+  function handleclick() {
+    contador++;
+  }
+</script>
+
+<main>
+  <button on:click={handleclick}>Incrementar</button>
+  <span>{contador}</span>
+</main>
+
+<style>
+  span {
+    color: red;
+  }
+</style>
